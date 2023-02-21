4a87560290f3b0e551806f4d4848c7e42d4a4557
chenjunjian <raydraw@163.com>
2023-02-21 09:25:59 +0000

    docker compose 启动

--- etcd.sh
+++ etcd.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sudo docker pull bitnami/etcd
-sudo docker run -it -d --name Etcd --env ALLOW_NONE_AUTHENTICATION=yes  bitnami/etcd:latest
+sudo docker run -it -d --name Etcd --env ALLOW_NONE_AUTHENTICATION=yes -p '2379:2379' bitnami/etcd:latest 
